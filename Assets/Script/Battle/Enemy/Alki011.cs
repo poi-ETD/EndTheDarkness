5c136f98b2ccc3618d9780701aa8aedf5d85b225
raetic <83604180+raetic@users.noreply.github.com>
2023-01-15 20:15:22 +0900

    115버그수정

--- Assets/Script/Battle/Enemy/Alki011.cs
+++ Assets/Script/Battle/Enemy/Alki011.cs
@@ -1,18 +1,150 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Alki011 : MonoBehaviour
+using UnityEngine.UI;
+using TMPro;
+public class Alki011 : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public int curTurn;
+    public Sprite sprite_idle;
+    public Sprite sprite_highlight;
+    [SerializeField] TextMeshProUGUI NameT;
+    private bool startPattern;
+    private int phase = 1;
+  private  int pattern = 1;
+    private bool pattern5;
+    private bool[] myAct=new bool[4];
+    public override void Start()
     {
-        
+        base.Start();
+        TM = GameObject.Find("TurnManager").GetComponent<TurnManager>();
+        BM = GameObject.Find("BattleManager").GetComponent<BattleManager>();
+        NameT.text = Name;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void EnemySelectPattern()
     {
-        
+        if (phase == 2 && Hp < 50) {
+            phase = 3;
+        }
+        if(phase==1&&status[0]>=10)
+        {
+            pattern5 = true;
+        }
+        base.EnemySelectPattern();
+        StartPattern();
+
+    }
+
+
+    void StartPattern()
+    {
+        if (BM.teamDieCount < BM.characters.Count)
+        {
+
+            if (!isDie)
+            {
+                if(phase==1)
+                {
+
+                    if (pattern5)
+                    {
+
+                    }
+                    else
+                    {
+                        int rand = Random.Range(0, 4);
+                        while (myAct[rand])
+                        {
+                            rand = Random.Range(0, 4);
+                        }
+                        if (rand == 0)
+                        {
+
+                        }
+                        if (rand == 1)
+                        {
+
+                        }
+                        if (rand == 2)
+                        {
+
+                        }
+                        if (rand == 3)
+                        {
+
+                        }
+                      
+                        if (myAct[0] && myAct[1] && myAct[3] && myAct[2])
+                        {
+                            for (int i = 0; i < 4; i++) myAct[rand] = false;
+                        }
+                    }
+                }
+                if(phase==2)
+                {
+                    if(startPattern)
+                    {
+                        startPattern = false;
+                    }
+                    else
+                    {
+                        int rand = Random.Range(0, 3);
+                        while (myAct[rand])
+                        {
+                            rand = Random.Range(0, 3);
+                        }
+                        if (rand == 0)
+                        {
+
+                        }
+                        if (rand == 1)
+                        {
+
+                        }
+                        if (rand == 2)
+                        {
+
+                        }
+                     
+                        if (myAct[0] && myAct[1] && myAct[2])
+                        {
+                            for (int i = 0; i < 3; i++) myAct[rand] = false;
+                        }
+                    }
+                }
+                if(phase==3)
+                {
+                    int rand = Random.Range(0, 4);
+                    while (myAct[rand])
+                    {
+                        rand = Random.Range(0, 4);
+                    }
+                    if (rand == 0)
+                    {
+
+                    }
+                    if (rand == 1)
+                    {
+
+                    }
+                    if (rand == 2)
+                    {
+
+                    }
+                    if (rand == 3)
+                    {
+
+                    }
+
+                    if (myAct[0] && myAct[1] && myAct[3] && myAct[2])
+                    {
+                        for (int i = 0; i < 4; i++) myAct[rand] = false;
+                    }
+                }
+            }
+            BM.AM.EnemyAct();
+        }
     }
 }
--- Assets/Script/Battle/Enemy/Jang010.cs
+++ Assets/Script/Battle/Enemy/Jang010.cs
@@ -1,18 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Jang010 : MonoBehaviour
+using UnityEngine.UI;
+using TMPro;
+public class Jang010 : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    public Image image_character;
+    public Sprite sprite_idle;
+    public Sprite sprite_highlight;
+    [SerializeField] TextMeshProUGUI NameT;
+    public int myAct;
+    public int phase = 1;
+
+    
+    public override void Start()
     {
-        
+        base.Start();
+        Name = "장";
+        NameT.text = Name;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void EnemySelectPattern()
     {
-        
+        base.EnemySelectPattern();
+        StartPattern();
+    }
+
+
+
+    void StartPattern()
+    {
+        if (BM.teamDieCount < BM.characters.Count)
+        {
+
+            if (!isDie)
+            {
+                if (phase == 1)
+                {
+                    int rand = Random.Range(0, 3);
+                    if (rand == 0)
+                    {
+                        for (int i = 0; i < 7; i++)
+                            BM.EnemyAttack(1, this, BM.SelectCharacterInEnemyTurn(0, 0));
+
+                    }
+                    if (rand==1)
+                    {
+                        for (int i = 0; i < 3; i++)
+                            BM.EnemyAttack(1, this, BM.SelectCharacterInEnemyTurn(0, 0));
+                        BM.EnemyAttack(1, this, BM.SelectCharacterInEnemyTurn(0, 1));
+                    }
+                    if (rand==2)
+                    {
+                        for (int i = 0; i < TM.turn*2; i++)
+                            BM.EnemyAttack(1, this, BM.SelectCharacterInEnemyTurn(0, 0));
+                    }
+
+                }
+                else//2페이즈
+                {
+                    int rand = Random.Range(0, 3);
+                    if (rand == 0)
+                    {
+                        for (int i = 0; i < TM.turn; i++)
+                            BM.EnemyAttack(1, this, BM.SelectCharacterInEnemyTurn(0, 0));
+
+                    }
+                    if (rand == 1)
+                    {
+                        for (int i = 0; i < 3; i++)
+                            BM.EnemyAttack(1, this, BM.SelectCharacterInEnemyTurn(0, 0));
+                        BM.EnemyAttack(1, this, BM.SelectCharacterInEnemyTurn(0, 1));
+                        BM.EnemyAttack(1, this, BM.SelectCharacterInEnemyTurn(0, 1));
+                    }
+                    if (rand == 2)
+                    {
+                        for (int i = 0; i < TM.turn * 2; i++)
+                            BM.EnemyAttack(1, this, BM.SelectCharacterInEnemyTurn(0, 0));
+                    }
+                }
+            }
+            BM.AM.EnemyAct();
+        }
     }
 }
